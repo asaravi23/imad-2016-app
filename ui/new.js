4a985538835704346f9520afb23b597411a53922
asaravi23 <asaravi23@gmail.com>
2016-10-27 13:01:03 +0530

    [imad-console] Updates ui/new.js

--- ui/new.js
+++ ui/new.js
@@ -1,6 +1,19 @@
-#first
+window.onload= function()
 {
-    height:400px;
-    width:500px;
-    position:absolute;
-}
+    var pos= 0;
+    var box1= document.getElementById('first');
+    var box2= document.getElementById('second');
+    var box3= document.getElementById('third');
+    
+    var t= setInterval(move,10);
+    
+    
+    function move()
+    {
+  
+        pos= pos+1;
+        box1.style.left= pos +'px';
+    }
+    
+    
+};
