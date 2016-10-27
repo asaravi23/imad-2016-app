12feec6ed3eac8e8929421884c4cf045ff26fabe
asaravi23 <asaravi23@gmail.com>
2016-10-27 23:16:16 +0530

    [imad-console] Updates ui/new.js

--- ui/new.js
+++ ui/new.js
@@ -14,6 +14,8 @@ var box3= document.getElementById('third');
 var a= [box1, box2, box3];
 var x=a[0];
 var y=0;
+var c;
+
 var t= setInterval(move,10);
     
     
@@ -31,16 +33,36 @@ x.style.visibility= "visible";
 
 if(pos>500)
 {y=y+1;
-x=a[y];
+
 pos=0;
+
+
+if(y>2)
+{
+hide();
+y=0;
+}
+x=a[y];
+
+}
 }
 
+
+function hide()
+{
+
+for(c=0;c<a.length;c++)
+{
+
+a[c].style.visibility="hidden";
+
+
 }
 
 
-
-
-
+}    
     
-    
+
+
+
 };
