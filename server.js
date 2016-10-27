8adaa1a44e43f35dc5297461d47ebd6c81e3a228
asaravi23 <asaravi23@gmail.com>
2016-10-27 23:15:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -85,8 +85,17 @@ app.get('/ui/index1.html', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
+
+app.get('/ui/new.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'new.css'));
+});
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
+
+app.get('/ui/new.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'new.js'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
